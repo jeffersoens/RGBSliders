dc91723b160adcc922acf3aa35e62443b2223742
Vyacheslav Gorbatenko <jeffersoens@gmail.com>
2022-11-16 20:45:13 +0300

    Fixed feedback issues

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var hexText: UITextField!
     
+    @IBOutlet weak var hexButton: UIButton!
+    
+    
+    // MARK: - Private Properties
+    
     private var currentRed: CGFloat = 1
     private var currentGreen: CGFloat = 0.5
     private var currentBlue: CGFloat = 0
@@ -32,104 +37,117 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setSlidersMaxMin()
-        showSlidersLabels()
-        showColorView(red: currentRed, green: currentGreen, blue: currentBlue)
+        updateSliderLabels()
+        updateColorView(
+            red: currentRed,
+            green: currentGreen,
+            blue: currentBlue
+        )
+        updateHexButton()
     }
     
-    // MARK: - Slider Actions
-    @IBAction func redSliderChanging() {
-        currentRed = CGFloat(redSlider.value)
-        showSlidersLabels()
-        showColorView(red: currentRed, green: currentGreen, blue: currentBlue)
-        hexText.text = colorView.backgroundColor?.toHexString()
-
+    // MARK: - IBActions
+    
+    @IBAction func sliderChanging(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            currentRed = CGFloat(redSlider.value)
+        case greenSlider:
+            currentGreen = CGFloat(greenSlider.value)
+        case blueSlider:
+            currentBlue = CGFloat(blueSlider.value)
+        default:
+            break
+        }
+        
+        updateSliderLabels()
+        updateColorView(
+            red: currentRed,
+            green: currentGreen,
+            blue: currentBlue
+        )
+        updateHexButton()
     }
     
-    @IBAction func greenSliderChanging() {
-        currentGreen = CGFloat(greenSlider.value)
-        showSlidersLabels()
-        showColorView(red: currentRed, green: currentGreen, blue: currentBlue)
-        hexText.text = colorView.backgroundColor?.toHexString()
-
+    
+    @IBAction func colorButtonTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            // red
+            colorButtonAction(red: 1, green: 0.22, blue: 0.2)
+        case 2:
+            // green
+            colorButtonAction(red: 0.2, green: 0.78, blue: 0.35)
+        case 3:
+            // blue
+            colorButtonAction(red: 0, green: 0.5, blue: 1)
+        case 4:
+            // orange
+            colorButtonAction(red: 1, green: 0.58, blue: 0)
+        case 5:
+            //mint
+            colorButtonAction(red: 0.35, green: 0.77, blue: 0.74)
+        case 6:
+            // purple
+            colorButtonAction(red: 0.64, green: 0.34, blue: 0.84)
+        case 7:
+            // yellow
+            colorButtonAction(red: 0.96, green: 0.8, blue: 0)
+        case 8:
+            // indigo
+            colorButtonAction(red: 0.31, green: 0.30, blue: 0.73)
+        case 9:
+            // cyan
+            colorButtonAction(red: 0.35, green: 0.6, blue: 0.79)
+        default:
+            break
+        }
     }
     
-    @IBAction func bueSliderChanging() {
-        currentBlue = CGFloat(blueSlider.value)
-        showSlidersLabels()
-        showColorView(red: currentRed, green: currentGreen, blue: currentBlue)
-        hexText.text = colorView.backgroundColor?.toHexString()
-
-    }
-    
-    //MARK: - Button Actions
-    @IBAction func setRedColorButton() {
-        setButtonColor(red: 1, green: 0.22, blue: 0.2)
-    }
-    
-    @IBAction func setGreenColorButton() {
-        setButtonColor(red: 0.2, green: 0.78, blue: 0.35)
-    }
-    
-    @IBAction func setBlueColorButton() {
-        setButtonColor(red: 0, green: 0.5, blue: 1)
-    }
-    
-    @IBAction func setOrangeColorButton() {
-        setButtonColor(red: 1, green: 0.58, blue: 0)
-    }
-    
-    @IBAction func setMintColorButton() {
-        setButtonColor(red: 0.35, green: 0.77, blue: 0.74)
-    }
-    
-    @IBAction func setPurpleColorButton() {
-        setButtonColor(red: 0.64, green: 0.34, blue: 0.84)
-    }
-    
-    @IBAction func setYellowColorButton() {
-        setButtonColor(red: 0.96, green: 0.8, blue: 0)
-    }
-    
-    @IBAction func setIndigoColorButton() {
-        setButtonColor(red: 0.31, green: 0.30, blue: 0.73)
-    }
-    
-    @IBAction func setCyanColorButton() {
-        setButtonColor(red: 0.35, green: 0.6, blue: 0.79)
-    }
-    
-    @IBAction func setRandonColorButton() {
-        setButtonColor(red: Float.random(in: 0...1), green: Float.random(in: 0...1), blue: Float.random(in: 0...1))
+    @IBAction func randomColorButtonTapped() {
+        colorButtonAction(
+            red: Float.random(in: 0...1),
+            green: Float.random(in: 0...1),
+            blue: Float.random(in: 0...1)
+        )
     }
     
 // MARK: - Private Methods
     
-    private func showColorView(red: CGFloat, green: CGFloat, blue: CGFloat) {
-        colorView.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
+    private func updateColorView(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        colorView.backgroundColor = UIColor.init(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1
+        )
         colorView.layer.borderWidth = 3
         colorView.layer.borderColor = UIColor.white.cgColor
     }
     
-    private func showSlidersLabels() {
+    private func updateSliderLabels() {
         redSliderLabel.text = String(format: "%.2f", redSlider.value)
         greenSliderLabel.text = String(format: "%.2f", greenSlider.value)
         blueSliderLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
-    private func setSlidersValues(red: Float, green: Float, blue: Float) {
+    private func setSliderValues(red: Float, green: Float, blue: Float) {
         redSlider.setValue(red, animated: true)
         greenSlider.setValue(green, animated: true)
         blueSlider.setValue(blue, animated: true)
     }
     
-    private func setButtonColor(red: Float, green: Float, blue: Float) {
-        showColorView(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue))
-        setSlidersValues(red: red, green: green, blue: blue)
-        showSlidersLabels()
-        hexText.text = colorView.backgroundColor?.toHexString()
+    private func colorButtonAction(red: Float, green: Float, blue: Float) {
+        updateColorView(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue)
+        )
+        setSliderValues(red: red, green: green, blue: blue)
+        updateSliderLabels()
+        updateHexButton()
     }
     
-    // словил баг, когда минимальное значение красного слайдера было 0.5, хотя в настройках стоял 0. Пришлось прописать в коде – только тогда заработало
     private func setSlidersMaxMin() {
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
@@ -138,10 +156,18 @@ class ViewController: UIViewController {
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
     }
+    
+    private func updateHexButton() {
+        hexButton.setTitle(
+            colorView.backgroundColor?.toHexString(),
+            for: .normal
+        )
+    }
 }
 
 extension UIColor {
         func toHexString() -> String {
+            // взял со StackOverflow :)
             var r:CGFloat = 0
             var g:CGFloat = 0
             var b:CGFloat = 0
